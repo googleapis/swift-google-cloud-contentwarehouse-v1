39b2c70cbf59dffb72cc310dc4f52e5c72794a3a
Jeff Ching <chingor@google.com>
2026-09-04 11:18:05 -0700

    deps: update wkt, auth dependencies to remote url (#545)
    Towards #450
    
    Also, updates the CI script to be able to use local code for testing.
    Example found failure: in #618

--- Package.swift
+++ Package.swift
@@ -24,10 +24,10 @@ let package = Package(
     .library(name: "GoogleCloudContentWarehouseV1", targets: ["GoogleCloudContentWarehouseV1"])
   ],
   dependencies: [
-    .package(path: "../../packages/swift-google-auth"),
+    .package(url: "https://github.com/googleapis/swift-google-auth", from: "0.0.0-preview"),
     .package(path: "../../generated/swift-google-cloud-documentai-v1"),
     .package(path: "../../packages/swift-google-gax"),
-    .package(path: "../../packages/swift-google-wkt"),
+    .package(url: "https://github.com/googleapis/swift-google-wkt", from: "0.1.0-preview"),
     .package(path: "../../generated/swift-google-iam-v1"),
     .package(path: "../../generated/swift-google-longrunning"),
     .package(path: "../../generated/swift-google-rpc"),
